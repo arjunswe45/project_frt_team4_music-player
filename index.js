16cd2a8a5c275b20df6688a6abd06d07007d3abb
Thangarasu <rthangarasu030@gmail.com>
2023-12-19 11:34:54 +0530

    Update

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("port", process.env.PORT || 5000);
 
 // Home page
-app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Home.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/music", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 
